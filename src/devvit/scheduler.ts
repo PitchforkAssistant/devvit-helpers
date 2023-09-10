9497729c5fd4ad0437a69ae8b9257209577d5029
PitchforkAssistant <pitchforkassistant@gmail.com>
2023-09-10 04:29:05 +0300

    add docstring to startSingletonJob

--- src/devvit/scheduler.ts
+++ src/devvit/scheduler.ts
@@ -2,9 +2,17 @@
  * @file This file contains helper functions that for the Devvit scheduler.
  */
 
-import {Scheduler} from "@devvit/public-api";
+import {Data, Scheduler} from "@devvit/public-api";
 
-export async function startSingletonJob (scheduler: Scheduler, jobName: string, cronSchedule: string, data: Record<string, unknown>): Promise<string> {
+/**
+ * This function lets you start a job that should only have one instance running at a time. It will cancel any existing instances of the job before starting a new one.
+ * @param scheduler An instance of the Scheduler object, such as context.scheduler from inside most Devvit event handlers.
+ * @param jobName The name of the job to start.
+ * @param cronSchedule The cron schedule to use for the job. Use https://crontab.guru/ to generate/validate a cron schedule if you're not familiar with cron.
+ * @param data The data to pass to the job, it can be any object.
+ * @returns The ID of the job that was started.
+ */
+export async function startSingletonJob (scheduler: Scheduler, jobName: string, cronSchedule: string, data: Data): Promise<string> {
     // Cancel existing instances of the job.
     const currentJobs = await scheduler.listJobs();
     for (const job of currentJobs) {
