6cbc612708791781be710307df478cd94e46f1ce
PitchforkAssistant <pitchforkassistant@gmail.com>
2023-09-13 14:30:15 +0300

    take string or string[] as mod names in hasPerformedActions

--- src/devvit/redditAPI.ts
+++ src/devvit/redditAPI.ts
@@ -4,6 +4,7 @@
 
 import {ModActionType, RedditAPIClient, Comment, Post} from "@devvit/public-api";
 import {getTimeDeltaInSeconds} from "../misc/date.js";
+import {valueToArrayOrUndefined} from "../misc/misc.js";
 
 /**
  * This function lets you check if moderators have performed a specific action on something.
@@ -11,13 +12,14 @@ import {getTimeDeltaInSeconds} from "../misc/date.js";
  * @param subredditName Subreddit name as a string (no prefix).
  * @param actionTargetId Action target ID, should be the prefixed ID of a post, comment, or user.
  * @param actionType Type of moderation action as one of the strings from ModActionType.
- * @param moderatorUsernames Limits the search to a list of moderator usernames (no prefix). Defaults to all moderators.
+ * @param moderators Limits the search to a moderator or list of moderator usernames (no prefix). Defaults to all moderators.
  * @param includeRelatives Also check whether the action has been performed on a post or any of its comments. Defaults to off.
  * @param cutoffSeconds Any actions older than this many seconds will not be considered a match. Defaults to off.
  * @param limit The number of actions to fetch. Defaults to 100, the maximum allowed in a single request.
  * @returns A boolean indicating whether the action has been performed.
  */
-export async function hasPerformedAction (reddit: RedditAPIClient, subredditName: string, actionTargetId: string, actionType: ModActionType, moderatorUsernames?: string[], includeParent?: boolean, cutoffSeconds?: number, limit = 100): Promise<boolean> {
+export async function hasPerformedAction (reddit: RedditAPIClient, subredditName: string, actionTargetId: string, actionType: ModActionType, moderators?: string | string[], includeParent?: boolean, cutoffSeconds?: number, limit = 100): Promise<boolean> {
+    const moderatorUsernames = valueToArrayOrUndefined<string>(moderators);
     const modLog = await reddit.getModerationLog({subredditName, moderatorUsernames, type: actionType, limit, pageSize: 100}).all().catch(e => {
         console.error(`Failed to fetch ${actionType} log for ${subredditName} by ${moderatorUsernames?.join(",") ?? ""}`, e);
         return [];
@@ -40,14 +42,14 @@ export async function hasPerformedAction (reddit: RedditAPIClient, subredditName
  * @param subredditName Subreddit name as a string (no prefix).
  * @param actionTargetId Action target ID, should be the prefixed ID of a post, comment, or user.
  * @param actionTypes List of moderation action types as strings from ModActionType.
- * @param moderatorUsernames Limits the search to a list of moderator usernames (no prefix). Defaults to all moderators.
+ * @param moderators Limits the search to a list of moderator usernames (no prefix). Defaults to all moderators.
  * @param includeRelatives Also check whether the action has been performed on a post or any of its comments. Defaults to off.
  * @param cutoffSeconds Any actions older than this many seconds will not be considered a match. Defaults to off.
  * @param limit The number of actions to fetch. Defaults to 100, the maximum allowed in a single request.
  * @returns A boolean indicating whether any of actions have been performed.
  */
-export async function hasPerformedActions (reddit: RedditAPIClient, subredditName: string, actionTargetId: string, actionTypes: ModActionType[], moderatorUsernames?: string[], includeParent?: boolean, cutoffSeconds?: number, limit = 100): Promise<boolean> {
-    const actionChecks = actionTypes.map(actionType => hasPerformedAction(reddit, subredditName, actionTargetId, actionType, moderatorUsernames, includeParent, cutoffSeconds, limit));
+export async function hasPerformedActions (reddit: RedditAPIClient, subredditName: string, actionTargetId: string, actionTypes: ModActionType[], moderators?: string | string[], includeParent?: boolean, cutoffSeconds?: number, limit = 100): Promise<boolean> {
+    const actionChecks = actionTypes.map(actionType => hasPerformedAction(reddit, subredditName, actionTargetId, actionType, moderators, includeParent, cutoffSeconds, limit));
     const results = await Promise.all(actionChecks);
     return results.includes(true);
 }
