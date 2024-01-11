d68630e58d5a63656aca037571575139cb4bc210
PitchforkAssistant <pitchforkassistant@gmail.com>
2024-01-11 12:02:25 +0200

    don't use extra new lines in onAnyTriggerConsoleLog

--- src/devvit/triggers.ts
+++ src/devvit/triggers.ts
@@ -12,6 +12,6 @@ import {OnTriggerEvent, TriggerContext, TriggerEvent, TriggerEventType} from "@d
 export async function onAnyTriggerConsoleLog (event: OnTriggerEvent<TriggerEventType[TriggerEvent]>, context: TriggerContext) {
     // A single log that's too long sometimes doesn't show up at all, so better to do these separately.
     console.log(`type: ${event.type}`);
-    console.log(`event:\n${JSON.stringify(event)}`);
-    console.log(`context:\n${JSON.stringify(context)}`);
+    console.log(`event: ${JSON.stringify(event)}`);
+    console.log(`context: ${JSON.stringify(context)}`);
 }
