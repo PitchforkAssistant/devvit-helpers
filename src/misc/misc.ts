8029b03a91c7e5d0beae9914507798a7d66b5f07
PitchforkAssistant <pitchforkassistant@gmail.com>
2024-12-13 08:16:28 +0200

    devvit 0.11.4

--- src/misc/misc.ts
+++ src/misc/misc.ts
@@ -22,7 +22,7 @@ export function domainFromUrlString (url: string, defaultValue = ""): string {
         } else {
             return defaultValue;
         }
-    } catch (error) {
+    } catch {
         return defaultValue;
     }
 }
