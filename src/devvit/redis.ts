45de02eb3c5ca3da41e6e1f35bbb3fddb46f36c0
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-11-12 12:53:53 +0000

    Add additional Redis helpers

--- src/devvit/redis.ts
+++ src/devvit/redis.ts
@@ -3,7 +3,9 @@
  */
 
 import {RedisFieldValue} from "@devvit/protos";
-import {RedisClient, ZMember} from "@devvit/public-api";
+import {RedisClient, ZMember, ZRangeOptions} from "@devvit/public-api";
+import { differenceInSeconds } from "date-fns";
+import { chunk } from "lodash";
 
 /**
  * Scans a sorted set in Redis and returns all members that match a given pattern. This function handles partial results and does multiple zScan calls if needed.
@@ -53,4 +55,82 @@ export async function hScanAll (redis: RedisClient, key: string, pattern: string
         cursor = hScanResult.cursor;
     }
     return Object.values(fields);
+}
+
+/**
+ * Retrieves multiple fields from a Redis hash and returns them as a record/object.
+ * @param {RedisClient} redis An instance of Devvit's Redis client.
+ * @param key The key of the hash.
+ * @param fields The fields to retrieve values for
+ * @returns  {Record<string, string>} An object with field-value pairs for the specified fields.
+ */
+export async function hMGetAsRecord (redis: RedisClient, key: string, fields: string[]): Promise<Record<string, string>> {
+    if (fields.length === 0) {
+        return {};
+    }
+
+    const results: Record<string, string> = {};
+
+    const values = await redis.hMGet(key, fields);
+
+    fields.forEach((field, index) => {
+        if (values[index] !== null) {
+            results[field] = values[index];
+        }
+    });
+
+    return results;
+}
+
+/**
+ * Get all entries from a hash in Redis, chunking the requests if necessary to avoid exceeding Devvit's limitations on response size.
+ * @param {RedisClient} redis An instance of Devvit's Redis client.
+ * @param key The key of the hash.
+ * @param chunkSize The size of each chunk.
+ * @returns {Record<string, string>} An object with field-value pairs for the specified fields.
+ */
+export async function hMGetAllChunked (redis: RedisClient, key: string, chunkSize = 5000): Promise<Record<string, string>> {
+    const fields = await redis.hKeys(key);
+    if (fields.length === 0) {
+        return {};
+    }
+
+    const results = await Promise.all(chunk(fields, chunkSize).map(c => hMGetAsRecord(redis, key, c)));
+
+    return Object.assign({}, ...results) as Record<string, string>;
+}
+
+/**
+ * Returns a range of members from a sorted set in Redis as a record/object, where the keys are the members and the values are their scores.
+ * @param {RedisClient} redis An instance of Devvit's Redis client.
+ * @param key The key of the sorted set.
+ * @param start The starting index (inclusive) of the range.
+ * @param stop The ending index (exclusive) of the range.
+ * @param options Optional parameters for the zRange command.
+ * @returns {Record<string, number>} A record/object with members and their scores.
+ */
+export async function zRangeAsRecord (redis: RedisClient, key: string, start: number | string, stop: number | string, options?: ZRangeOptions): Promise<Record<string, number>> {
+    const results: Record<string, number> = {};
+    const items = await redis.zRange(key, start, stop, options);
+
+    items.forEach((item) => {
+        results[item.member] = item.score;
+    });
+
+    return results;
+}
+
+/**
+ * Sets an expiration time on a Redis key based on a Date object.
+ * @param {RedisClient} redis An instance of Devvit's Redis client.
+ * @param key The redis key.
+ * @param expireAt The expiration date.
+ */
+export async function expireKeyAt (redis: RedisClient, key: string, expireAt: Date) {
+    const secondsUntil = differenceInSeconds(expireAt, new Date());
+    if (secondsUntil > 0) {
+        await redis.expire(key, secondsUntil);
+    } else {
+        throw new Error(`Expire time ${expireAt.toISOString()} is in the past`);
+    }
 }
